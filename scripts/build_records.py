98fa2d033e5bd75746abf8e3a903e4d9583e4479
KoBeWa <benneyftw@gmail.com>
2025-12-17 13:20:23 +0100

    Create build_records.py

---
+++ scripts/build_records.py
@@ -0,0 +1,9 @@
+PF_CANDIDATES = [
+    "points", "score", "pf", "points_for", "points_scored", "team_points", "team_score",
+    "total"  # <-- NEU (dein CSV)
+]
+
+PA_CANDIDATES = [
+    "opponentpoints", "opp_points", "opp_score", "pa", "points_against", "opponent_score", "opponent_points",
+    "opponent_total"  # <-- NEU (dein CSV)
+]
